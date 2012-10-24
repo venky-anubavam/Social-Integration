64ac57ecd3ce00101c0212039d056625d3903baa
unknown <Anu-Venky@admin.(none)>
2012-10-24 17:35:43 +0530

    Application name and id changes

--- Resources/app.js
+++ Resources/app.js
@@ -1,18 +1,8 @@
 /**
  * This is a really simple social app that demonstrates how to post to Twitter using Appcelerator Titanium.
- * REQUIRED: DOWNLOAD the "social.js" file from http://appc.me/social.js
- *           and SAVE IT in your app's Resources directory.
- *
+ * 
  * Get API access to Twitter here: https://dev.twitter.com/apps
  * The consumer key and secret will be used later in this app. Make sure you check "Read & Write"!
- *
- * The latest version of this file can always be found at http://appc.me/social.sample.js
- *
- * Tweet me @dawsontoth with questions, or on the Help Desk if you're an Appcelerator customer!
- */
-
-/**
- * We'll start by making a window with a button...
  */
 
 Ti.include('fb.js');
